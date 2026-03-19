57dd97b6ed33f848143dcb8cea0c5212b8e42e97
John Albert A. Paulino <johnalbert.paulino@neu.edu.ph>
2026-03-19 14:37:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1509472913-18bd1",
+  "appId": "1:964054861004:web:81aee815f046e69e502e69",
+  "apiKey": "AIzaSyBMfEgCBsfSPUOcaKnDLQTtV5natPgOQAg",
+  "authDomain": "studio-1509472913-18bd1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "964054861004"
+};
